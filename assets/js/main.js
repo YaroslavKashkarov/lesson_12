c819ee6785e25b3722ba3a2c61fa1d70209e6ded
Yaroslav Kashkarov <yaroslavkashkarov@gmail.com>
2023-04-01 13:31:33 +0200

    Добавление проекта Bootstrap

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+
+
+$(document).ready(function () {
+  $('.header-navbar__btn').on('click', function () {
+    $(this).toggleClass('-active');
+    $('.header-navbar__list').stop(true, true).slideToggle(500);
+  });
+});
